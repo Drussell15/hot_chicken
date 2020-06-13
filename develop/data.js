889f507e2ba58b0d8c2733582c2979b0b469fab8
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-06-13 13:02:52 -0700

    merged server and data file

--- develop/data.js
+++ develop/data.js
@@ -1,31 +1,55 @@
 const http = require("http");
 const PORT = 3000;
 const path = require("path");
-
+const express = require("express");
+const app = express();
 const server = http.createServer(handleRequest);
 
-function handleRequest(req, res) {
-    let requestData = "";
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+// Dependencies
 
-    req.on("data", function(data) {
-        requestData += data;  
+
+// Data
+const reservations = [{
+  name:
+    number:
+  email:
+    uniqueID:
+}];
+
+const waitList = [{
+  name:
+    number:
+  email:
+    uniqueID:
+}];
+
+function handleRequest(req, res) {
+  let requestData = "";
+
+  req.on("data", function (data) {
+    requestData += data;
+  });
+
+  req.on("display", function () {
+
+    app.get("/", function (req, res) {
+      // add html file homepage
+      res.sendFile(path.join(__dirname, "view", "home-page.html"));
     });
 
-    req.on("display", function() {
+    app.get("/viewTables", function (req, res) {
+      // add html file
+      res.sendFile(path.join(__dirname, "view", ".html"));
+    });
 
-        app.get("/", function(req, res) {
-            // add html file homepage
-            res.sendFile(path.join(__dirname,"view", "home-page.html"));
-          });
-          
-          app.get("/viewTables", function(req, res) {
-            // add html file
-            res.sendFile(path.join(__dirname,"view", ".html"));
-          });
-        
-        app.get("/reservations", function(req, res) {
-                // add html file
-                res.sendFile(path.join(__dirname,"view", ".html"));
-            });
-    })
-}
+    app.get("/reservations", function (req, res) {
+      // add html file
+      res.sendFile(path.join(__dirname, "view", ".html"));
+    });
+  })
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  }
